f7007834751c2b779df7ef78affd0995bba69260
Robiul Islam <robiulislam.me2022@gmail.com>
2024-07-17 23:55:35 +0600

    add react form

--- src/components/pages/SignUp/SignUp.jsx
+++ src/components/pages/SignUp/SignUp.jsx
@@ -5,7 +5,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { useState } from "react";
 import useAuth from "../../Hooks/useAuth";
-import { Controller } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select"
 
 const SignUp = () => {
     const [message, setMessage] = useState();
@@ -13,32 +14,66 @@ const SignUp = () => {
     const axiosSecure = useAxiosSecure();
     const { singUpUser } = useAuth();
 
-    const handleUserInfo = event => {
-        event.preventDefault();
-        const form = new FormData(event.currentTarget);
 
-        const name = form.get('name');
-        const email = form.get('email');
-        const pin = form.get('pin');
-        const mobileNumber = form.get('mobile');
 
-        const userData = {
-            name: name,
-            email: email,
-            pin: pin,
-            mobileNumber: mobileNumber,
-            status: 'pending'
+    // const handleUserInfo = event => {
+    //     event.preventDefault();
+    //     const form = new FormData(event.currentTarget);
+
+    //     const name = form.get('name');
+    //     const email = form.get('email');
+    //     const pin = form.get('pin');
+    //     const mobileNumber = form.get('mobile');
+
+    //     const userData = {
+    //         name: name,
+    //         email: email,
+    //         pin: pin,
+    //         mobileNumber: mobileNumber,
+    //         status: 'pending'
+    //     }
+
+    //     if (pin.length !== 5 || !/^\d{5}$/.test(pin)) {
+    //         return setMessage('PIN must be a 5-digit number');
+    //     }
+
+    //     const firebasePass = pin + '0';
+    //     console.log(firebasePass, firebasePass.length);
+    //     singUpUser(email, firebasePass)
+    //         .then(() => {
+    //             axiosSecure.post('/users', userData)
+    //                 .then(res => {
+    //                     console.log(res.data);
+    //                     if (res.data.insertedId) {
+    //                         toast.success("User Sign In Successful")
+    //                         navigate('/dhome');
+    //                     }
+    //                 })
+    //         })
+
+    // }
+
+
+    const { register, handleSubmit, reset, control } = useForm(
+        {
+            userRole: 'Select Role',
         }
+    );
 
-        if (pin.length !== 5 || !/^\d{5}$/.test(pin)) {
+    const onSubmit = (data, e) => {
+        e.preventDefault();
+        console.log(data);
+        const userRole = data.userRole?.value;
+
+        if (data?.pin.length !== 5 || !/^\d{5}$/.test(data?.pin)) {
             return setMessage('PIN must be a 5-digit number');
         }
 
-        const firebasePass = pin + '0';
+        const firebasePass = data.pin + '0';
         console.log(firebasePass, firebasePass.length);
-        singUpUser(email, firebasePass)
+        singUpUser(data.email, firebasePass)
             .then(() => {
-                axiosSecure.post('/users', userData)
+                axiosSecure.post('/users', data)
                     .then(res => {
                         console.log(res.data);
                         if (res.data.insertedId) {
@@ -48,13 +83,65 @@ const SignUp = () => {
                     })
             })
 
-    }
+    };
 
 
 
     return (
         <div>
-            <div className="max-w-[800px] mx-auto mt-12">
+            <div>
+                <ToastContainer />
+                <div className="mt-12">
+                    <div className="text-center ">
+                        <h2 className="text-3xl font-bold font-serif ">RapidFunds</h2>
+                    </div>
+
+                    <div className='w-[700px] mx-auto mb-8'>
+                        <form
+                            onSubmit={handleSubmit(onSubmit)}
+                            className="card-body">
+                            <div className="form-control">
+                                <input type="text" placeholder="Your Name" name='name' {...register("name")} className="input input-bordered w-full shadow-md" />
+                            </div>
+                            <div className="form-control">
+                                <input type="email" placeholder="Your Email" name='email' {...register("email")} className="input input-bordered shadow-md" />
+                            </div>
+                            <div className="form-control">
+                                <input type="password" placeholder="Your pin" name='picture' {...register("pin")} className="input input-bordered shadow-md" />
+                            </div>
+
+                            <div className="form-control">
+                                <input type="number" name='number' placeholder="Mobile Number" {...register("number")} className="input input-bordered shadow-md" />
+
+                            </div>
+
+                            <div className="shadow-md">
+                                <Controller
+                                    name="userRole"
+                                    control={control}
+                                    render={({ field }) => (
+                                        <Select
+                                            {...field}
+                                            options={[
+                                                { label: "User" },
+                                                { label: "Agent" },
+                                            ]}
+                                        />
+                                    )}
+                                />
+                            </div>
+
+                            <div className="form-control mt-6">
+                                <button type='submit' className="btn btn-outline  btn-success border border-b-4">Sing Up</button>
+                            </div>
+                            <div>
+                                <p className="text-red-800">{message}</p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="max-w-[800px] mx-auto mt-12">
                 <h2 className="text-4xl font-bold text-center"> Registration Here</h2>
                 <div>
                     <form
@@ -108,8 +195,7 @@ const SignUp = () => {
                         <p className="text-red-800 font-semibold">{message}</p>
                     </div>
                 </div>
-            </div>
-            <ToastContainer />
+            </div> */}
         </div>
     );
 };
